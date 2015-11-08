ce5c61809a774e33edf4e926b62d69a913bf2635
TheOrangePotato <awsomeaust@gmail.com>
2015-11-07 21:26:28 -0800

    Created no-member-function hash header

---
+++ HeaderFiles/HashTable.h
@@ -0,0 +1,26 @@
+/*****
+HashTable Class
+Written by: Austin Bohannon
+
+Made to work with pointers to objects so that no data has to be copied,
+it's just organized.
+*****/
+template <class T>
+class HashTable
+{
+    T * arr[];
+    int arraySize, filledSlots, collisions, count;
+public:
+    //Constructor - Destructor
+    HashTable(int & setSize);
+    ~HashTable();
+    //Mutators and accessors
+    void addEntry(T * newEntry);
+    bool remove(T * deletePtr);
+    bool search(int & id, T * returnedPtr);
+    //Const accessors
+    int getSize() const {return arraySize;}
+    int getFilledSlots() const {return filledSlots;}
+    int getCollisions() const {return collisions;}
+    int getCount() const {return count;}
+}
