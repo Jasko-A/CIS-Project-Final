bff55ce95060e81e90043c6d8d674aa6cae7d6f9
theorangepotato <theorangepotato@users.noreply.github.com>
2015-11-17 16:04:55 -0800

    Created blank main.cpp with inlcudes

---
+++ SourceFiles/main.cpp
@@ -0,0 +1,16 @@
+/**
+Main file
+**/
+
+#include <iostream>
+#include <string>
+#include "Food.h"
+#include "BinarySearchTree.h"
+#include "HashTable.h"
+
+using namespace std;
+
+int main()
+{
+
+}
