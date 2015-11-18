c5864a39ee77e4887c8226ddab3e3a72e455201b
Jasko-A <Jasko@ATC203-A02.business-cs.deanza.ad.fhda.edu>
2015-11-17 16:53:07 -0800

    Started the main (barely)

--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -4,6 +4,7 @@ Main file
 
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "../HeaderFiles/Food.h"
 #include "../HeaderFiles/BinarySearchTree.h"
 #include "../HeaderFiles/HashTable.h"
@@ -12,5 +13,16 @@ using namespace std;
 
 int main()
 {
+	cout << "Welcome to the USDA\n\n";
+	fstream inFile;
+	inFile.open("USDA_data_small.txt");
+	if (inFile)
+	{
+		Food f;
+		inFile
+	}
 
+
+
+	return 0;
 }
