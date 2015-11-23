60721865723d640be2ee0207aa31c7a9ab745103
Jasko-A <jazzi96.ja@gmail.com>
2015-11-22 22:55:11 -0800

    Started the menu and witch statement in main

--- SourceFiles/main.cpp
+++ SourceFiles/main.cpp
@@ -5,24 +5,37 @@ Main file
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
 #include "../HeaderFiles/Food.h"
 #include "../HeaderFiles/BinarySearchTree.h"
 #include "../HeaderFiles/HashTable.h"
 
 using namespace std;
 
+void menu();
+
 int main()
 {
 	cout << "Welcome to the USDA\n\n";
 	fstream inFile;
+	string line;
 	inFile.open("USDA_data_small.txt");
 	if (inFile)
 	{
-		Food f;
-		inFile
+		while (inFile >> line)
+		{
+			//here we must parse the string into bits of data that we need for the nodes
+		}
+		
+		
+		
 	}
 
-
-
+	menu();
 	return 0;
+}
+
+void menu()
+{
+	cout << "\tWelcome to the USDA Nutrition Fact Sorter\tby:\t" << " Jasmin Adzic\n" << setw(81) << "Austin Bohannon\n" << setw(82) << "Brandon Archbold\n" << setw(80) << "Mikhail Smelik\n" << setw(71) << "Ahmed\n";
 }
