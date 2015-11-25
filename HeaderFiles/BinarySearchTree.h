a5b5e57c12e73505e56860934f82918add1820b5
brandon-a <babrandon21@gmail.com>
2015-11-25 15:19:09 -0800

    Removed 2nd BST class, added key & name functions
    Added functions for the BST class to work with unique keys and names
    separately

--- HeaderFiles/BinarySearchTree.h
+++ HeaderFiles/BinarySearchTree.h
@@ -11,16 +11,22 @@ class BinarySearchTree : public BinaryTree
 
 public:
 	// insert a node at the correct location
-	bool insert(Food * newEntry);
+	bool insert(Food * newEntry);			//BST sorted by ID
+	bool insertName(Food * newEntry);		//BST sorted by name
 	// remove a node if found
-	bool remove(int key);
+	bool remove(int key);					//remove by key
+	bool remove(string name);				//remove by Name
 	// find a target node
-	Food* search(int key) const;
+	Food* search(int key) const;			//Search by key
+	Food* search(string name) const;		//Search by name
 
 private:
 	BinaryNode* _insert(BinaryNode* nodePtr, BinaryNode* newNodePtr);
+	BinaryNode* _insertName(BinaryNode* nodePtr, BinaryNode* newNodePtr);
 	BinaryNode* _remove(BinaryNode* nodePtr, Food *& target, bool & success);
+	BinaryNode* _removeName(BinaryNode* nodePtr, Food *& target, bool & success);
 	BinaryNode* _search(BinaryNode* nodePtr, const Food & target) const;
+	BinaryNode* _searchName(BinaryNode* nodePtr, const Food & target) const;
 	BinaryNode* deleteNode(BinaryNode* nodePtr);
 	BinaryNode* removeLeftmostNode(BinaryNode* nodePtr, Food & successor);
 };
--- SourceFiles/BinarySearchTree.cpp
+++ SourceFiles/BinarySearchTree.cpp
@@ -176,3 +176,129 @@ BinaryNode* BinarySearchTree::_search(BinaryNode* nodePtr, const Food & target) 
 	}
 	return nodePtr;
 }
+
+
+// Sorting the BST by NAME
+/** =================================== */
+/* This is the public function which calls _insert to insert a new value in the BST.
+insert takes a pointer to a Food obeject to be inserted into the tree.
+The function inserts the item into the tree and then returns true to signify completion. */
+bool BinarySearchTree::insertName(Food * newNode)
+{
+	BinaryNode* newFood = new BinaryNode(newNode);
+	rootPtr = _insertName(rootPtr, newFood);
+	count++;
+	return true;
+}
+
+/** =================================== */
+/* This is the privite member function called by insert to access
+private member data recursively and insert a new node into the BST.
+Accepts nodePtr which is the root of the current tree. Accepts newNodePtr which is a pointer to the newNode to be inserted.
+The function returns the item that was inserted. If the item was not inserted successfully null is returned. */
+BinaryNode* BinarySearchTree::_insertName(BinaryNode* nodePtr, BinaryNode* newNodePtr)
+{
+	if (nodePtr == 0)
+	{
+		nodePtr = newNodePtr;
+	}
+	else if (newNodePtr->getItem()->getName() < nodePtr->getItem()->getName())
+	{
+		if (nodePtr->getLeftPtr())
+			_insert(nodePtr->getLeftPtr(), newNodePtr);
+		else
+			nodePtr->setLeftPtr(newNodePtr);
+	}
+	else
+	{
+		if (nodePtr->getRightPtr())
+			_insert(nodePtr->getRightPtr(), newNodePtr);
+		else
+			nodePtr->setRightPtr(newNodePtr);
+	}
+	return nodePtr;
+}
+
+/** =================================== */
+/* Public remove function to remove a node from the BST by calling _remove.
+Accepts the name of the item to be removed.
+If the name is found the item is removed and true is returned. If not false is returned. */
+bool BinarySearchTree::remove(string name)
+{
+	Food temp;
+	temp.setName(name);
+	Food * target = &temp;
+	bool isSuccessful = false;
+	rootPtr = _remove(rootPtr, target, isSuccessful);
+	return isSuccessful;
+}
+
+/** =================================== */
+/* Private member function to remove a node from the BST called by remove.
+Accepts a pointer to the root of the tree, the target that should be removed and a success flag to determine if the value is null or not.
+This function returns the root of the tree when finished. */
+BinaryNode* BinarySearchTree::_removeName(BinaryNode* nodePtr, Food *& target, bool & success)
+{
+	if (nodePtr == 0)
+	{
+		success = false;
+		return 0;
+	}
+	if (nodePtr->getItem()->getName() > target->getName())
+		nodePtr->setLeftPtr(_removeName(nodePtr->getLeftPtr(), target, success));
+	else if (nodePtr->getItem()->getName() < target->getName())
+		nodePtr->setRightPtr(_removeName(nodePtr->getRightPtr(), target, success));
+	else
+	{
+		nodePtr = deleteNode(nodePtr);
+		success = true;
+		count--;
+	}
+	return nodePtr;
+}
+
+/** == == == == == == == == == == == == == == == == == = */
+/* Public search function to find an item in the BST by calling _searchName.
+Accepts a int key for the item to be searched in the BST.
+This function returns a pointer to the Food object if found, else null is returned. */
+Food* BinarySearchTree::search(string name) const
+{
+	Food temp;
+	temp.setName(name);
+	Food * target = &temp;
+	BinaryNode* newNodePtr = _search(rootPtr, *target);
+	if (newNodePtr)
+	{
+		return newNodePtr->getItem();
+	}
+	else
+		return nullptr;
+}
+
+/** =================================== */
+/* Private member function called by search in order to find a specific node.
+Accepts the root of the BST as well as the target that is being searched for.
+Returns the Node of the item that is found. If not found null is returned. */
+BinaryNode* BinarySearchTree::_searchName(BinaryNode* nodePtr, const Food & target) const
+{
+	if (!nodePtr)
+		return nullptr;
+	else if (target.getName() == nodePtr->getItem()->getName())
+		return nodePtr;
+	else if (target.getName() < nodePtr->getItem()->getName())
+	{
+		if (nodePtr->getLeftPtr() == nullptr)
+			return nullptr;
+		else
+			return _search(nodePtr->getLeftPtr(), target);
+	}
+	else if (target.getName() > nodePtr->getItem()->getName())
+	{
+		if (nodePtr->getRightPtr())
+			return _search(nodePtr->getRightPtr(), target);
+		else
+			return nullptr;
+
+	}
+	return nodePtr;
+}
