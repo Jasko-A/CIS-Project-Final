57dc8c4e38e900805eecd343e490ee19a978417c
michaelsmelik <michaelsmelik@gmail.com>
2015-12-04 20:04:09 -0800

    Added comments

--- HeaderFiles/InputOutput.h
+++ HeaderFiles/InputOutput.h
@@ -22,9 +22,9 @@ are made.
 #include "../HeaderFiles/HashTable.h"
 using namespace std;
 
-int fileSize(string fileName);																					//To count the lines for the HashTable
-int hashSize(int fileSize);																						//To calculate hash size from number of lines
-Food* fileInput(ifstream &inFile);																				//To get the data from the file
+int fileSize(string fileName);																				//To count the lines for the HashTable
+int hashSize(int fileSize);																					//To calculate hash size from number of lines
+Food* fileInput(ifstream &inFile);																			//To get the data from the file
 void createADTs(ifstream &inFile, BinarySearchTree &keyBST, BinarySearchTree &secBST, HashTable *hTable);		//To put the data in the ADTs
 void emptyADTs(string fileName, BinarySearchTree &keyBST, BinarySearchTree &secBST, HashTable *hTable);			//To clear the data in the ADTs
 void printToFile(ofstream &outFile, BinarySearchTree &BST);               //To print to a file
